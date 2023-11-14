f781f7bcef5f174c54302c5031333cf85a0f404e
Zachariah Freitas <59587801+zachfreitas@users.noreply.github.com>
2023-11-13 17:22:08 -0800

    add new lessons

---
+++ Chapter_04_FUNCTIONS-SCOPING-AND-ABSTRACTION/04.05.01_circle.py
@@ -0,0 +1,9 @@
+# import a module called circle that we created 
+
+import circle 
+pi = 3 
+print(pi) 
+print(circle.pi) 
+print(circle.area(3)) 
+print(circle.circumference(3)) 
+print(circle.sphereSurface(3))
